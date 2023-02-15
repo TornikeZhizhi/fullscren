8a4948c8c6455963cebcec383ac8926f9a3f4bfc
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2023-02-15 17:11:29 +0400

    s

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
-let touchstartX = 0
-let touchendX = 0
+// let touchstartX = 0
+// let touchendX = 0
     
-function checkDirection() {
-    alert(touchendX,touchstartX)
-  if (touchendX < touchstartX) alert('swiped left!')
-  if (touchendX > touchstartX) alert('swiped right!')
-}
+// function checkDirection() {
+//     alert(touchendX,touchstartX)
+//   if (touchendX < touchstartX) alert('swiped left!')
+//   if (touchendX > touchstartX) alert('swiped right!')
+// }
 
-document.getElementById("test").addEventListener('touchstart', e => {
-    // alert("s")
-  touchstartX = e.changedTouches[0].screenX
+// document.getElementById("test").addEventListener('touchstart', e => {
+//     // alert("s")
+//   touchstartX = e.changedTouches[0].screenX
+// })
+
+// document.getElementById("test").addEventListener('touchend', e => {
+//   touchendX = e.changedTouches[0].screenX
+//   checkDirection()
+// })
+
+
+$("#test").click(function(){
+
+  console.log("ss")
 })
-
-document.getElementById("test").addEventListener('touchend', e => {
-  touchendX = e.changedTouches[0].screenX
-  checkDirection()
-})
-
-
 // document.getElementById("test").addEventListener('touchstart', (event) => {alert("ff")});
